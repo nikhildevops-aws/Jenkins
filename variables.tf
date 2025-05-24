528520c7d1cefc7f327bfb1e941c5ab8da53d18c
Nikhil Naphade <nikhilanaphade@gmail.com>
2025-05-24 21:38:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,40 @@
-variable "region" { default = "us-east-1" }
-variable "vpc_cidr" { default = "10.0.0.0/16" }
-variable "public_subnet_cidr" { default = "10.0.1.0/24" }
-variable "private_subnet_cidr" { default = "10.0.2.0/24" }
-variable "instance_type" { default = "t2.micro" }
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair to use for EC2 instances"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment name (e.g., dev, prod)"
+  type        = string
+  default     = "dev"
+}
